ef7664660f7ef11e515b6273e00d144d10fa24be
Mats Rynge <mats@rynge.net>
2017-06-07 12:55:32 -0700

    Added reboot hack

--- jetstream/salt/auto-update/auto-update.sh
+++ jetstream/salt/auto-update/auto-update.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-find /root/ -maxdepth 1 -name .ariella.auto-update -mmin +60 -exec rm -f {} \;
-if [ -e /root/.ariella.auto-update ]; then
+# hack - we need an extra reboot to get the disk correctly resized
+if [ ! -e /tmp/.ariella.reboot.1 ]; then
+    touch /tmp/.ariella.reboot.1
+    cd /srv/ariella/jetstream
+    ./bootstrap.sh
+    reboot
+    exit 0
+fi
+
+find /tmp/ -maxdepth 1 -name .ariella.auto-update -mmin +60 -exec rm -f {} \;
+if [ -e /tmp/.ariella.auto-update ]; then
     exit 0
 fi
 
 cd /srv/ariella
 git pull
 
-touch /root/.ariella.auto-update
+touch /tmp/.ariella.auto-update
 
 
