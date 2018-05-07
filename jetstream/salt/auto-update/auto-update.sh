ed66ca1a240c370afce69d3bd467b4fb619a7969
Mats Rynge <mats@rynge.net>
2018-05-07 15:29:30 -0700

    Removed the automatic reboot

--- jetstream/salt/auto-update/auto-update.sh
+++ jetstream/salt/auto-update/auto-update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # hack - we need an extra reboot to get the disk correctly resized
-if [ ! -e /tmp/.ariella.reboot.1 ]; then
-    touch /tmp/.ariella.reboot.1
-    cd /srv/ariella
-    git fetch --all
-    git reset --hard origin/master
-
-    cd /srv/ariella/jetstream
-    ./bootstrap.sh
-    reboot
-    exit 0
-fi
+#if [ ! -e /tmp/.ariella.reboot.1 ]; then
+#    touch /tmp/.ariella.reboot.1
+#    cd /srv/ariella
+#    git fetch --all
+#    git reset --hard origin/master
+#
+#    cd /srv/ariella/jetstream
+#    ./bootstrap.sh
+#    reboot
+#    exit 0
+#fi
 
 find /tmp/ -maxdepth 1 -name .ariella.auto-update -mmin +60 -exec rm -f {} \;
 if [ -e /tmp/.ariella.auto-update ]; then
